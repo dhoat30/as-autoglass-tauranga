ae6ec1b311d7e67e7f2469ea4331c20a9693b82c
dhoat30 <dhoat30@gmail.com>
2026-02-27 11:47:28 +1300

    added path

--- components/UI/Footer/FooterContact.jsx
+++ components/UI/Footer/FooterContact.jsx
@@ -1,20 +1,20 @@
-export const contactInfo = { 
-    info:[ 
-        {
-            url: "tel:022 588 6672", 
-            label: "022 588 6672",
-            icon: { 
-                url: "/phone.png", 
-                alt: "phone icon" 
-            },
-        },
-        {
-            url: "mailto:asglass2024@gmail.com", 
-            label: "asglass2024@gmail.com", 
-            icon: { 
-                url: "/email.png", 
-                alt: "Email icon" 
-            },
-        }
-    ]
-}
+export const contactInfo = {
+  info: [
+    {
+      url: "tel:022 588 6672",
+      label: "022 588 6672",
+      icon: {
+        url: "/phone.png",
+        alt: "phone icon",
+      },
+    },
+    {
+      url: "mailto:asglass0224@gmail.com",
+      label: "asglass0224@gmail.com",
+      icon: {
+        url: "/email.png",
+        alt: "Email icon",
+      },
+    },
+  ],
+};
